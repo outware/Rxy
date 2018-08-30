58a2d278fdf6c5866838a807c35ba8648ffc4dc0
Derek Clarkson <d4rkf1br3@gmail.com>
2018-08-30 14:27:30 +1000

    Initial Commit

---
+++ Rxy/Rxy.h
@@ -0,0 +1,19 @@
+//
+//  Rxy.h
+//  Rxy
+//
+//  Created by Derek Clarkson on 30/8/18.
+//  Copyright © 2018 Derek Clarkson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Rxy.
+FOUNDATION_EXPORT double RxyVersionNumber;
+
+//! Project version string for Rxy.
+FOUNDATION_EXPORT const unsigned char RxyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rxy/PublicHeader.h>
+
+
