de0e443aa4f5b9e8fd8623f83d1ba0100d44cb56
Derek Clarkson <d4rkf1br3@gmail.com>
2018-09-03 17:34:14 +1000

    Testing results

--- Rxy/MockResult.swift
+++ Rxy/MockResult.swift
@@ -3,12 +3,14 @@
 
 import RxSwift
 
+// MARK: - SingleResult
+
 public final class SingleResult<T> {
     
     private var error: Error?
     private var valueClosure: (() -> T)?
 
-    // MARK - Lifecycle
+    // MARK Lifecycle
     
     private init(error: Error) {
         self.error = error
@@ -22,34 +24,115 @@ public final class SingleResult<T> {
         self.valueClosure = value
     }
 
-    // MARK: - Factory methods
+    // MARK: Factory methods
     
-    public static func `throw`(_ error: Error) -> Self {
-        return self.init(error: error)
+    public static func `throw`(_ error: Error) -> SingleResult<T> {
+        return SingleResult<T>(error: error)
     }
 
     public static func `value`(_ value: T) -> SingleResult<T> {
-        return SingleResult(value: value)
+        return SingleResult<T>(value: value)
     }
 
     public static func `value`(_ value: @escaping () -> T) -> SingleResult<T> {
         return SingleResult(value: value)
     }
 
-    // MARK: - Tasks
+    // MARK: Tasks
     
     func resolve() -> Single<T> {
         if let valueClosure = valueClosure {
             return Single.just(valueClosure())
         }
-        return Single.error(RxyError.wrongType)
+        return Single.error(self.error!)
     }
 }
 
-//public final class CompletableResult: MockResult {
-//
-//}
-//
-//public final class ObservableResult: MockResult {
-//
-//}
+// MARK: - CompletableResult
+
+public final class CompletableResult {
+    
+    private var error: Error?
+    
+    // MARK Lifecycle
+    
+    private init(error: Error) {
+        self.error = error
+    }
+    
+    private init() {}
+    
+    // MARK: Factory methods
+    
+    public static func `throw`(_ error: Error) -> CompletableResult {
+        return self.init(error: error)
+    }
+    
+    public static func success() -> CompletableResult {
+        return CompletableResult()
+    }
+    
+    // MARK: Tasks
+    
+    func resolve() -> Completable {
+        if let error = self.error {
+            return Completable.error(error)
+        }
+        return Completable.empty()
+    }
+}
+
+// MARK: - MaybeResult
+
+public final class MaybeResult<T> {
+    
+    private var error: Error?
+    private var valueClosure: (() -> T)?
+    
+    // MARK Lifecycle
+
+    private init() {}
+
+    private init(error: Error) {
+        self.error = error
+    }
+    
+    private init(value: @autoclosure @escaping () -> T) {
+        self.valueClosure = value
+    }
+    
+    private init(value: @escaping () -> T) {
+        self.valueClosure = value
+    }
+    
+    // MARK: Factory methods
+    
+    public static func success() -> MaybeResult<T> {
+        return MaybeResult<T>()
+    }
+    
+    public static func `throw`(_ error: Error) -> MaybeResult<T> {
+        return MaybeResult<T>(error: error)
+    }
+    
+    public static func `value`(_ value: T) -> MaybeResult<T> {
+        return MaybeResult<T>(value: value)
+    }
+    
+    public static func `value`(_ value: @escaping () -> T) -> MaybeResult<T> {
+        return MaybeResult(value: value)
+    }
+    
+    // MARK: Tasks
+    
+    func resolve() -> Maybe<T> {
+        if let valueClosure = valueClosure {
+            return Maybe.just(valueClosure())
+        }
+        if let error = self.error {
+            return Maybe.error(error)
+        }
+        return Maybe.empty()
+    }
+}
+
--- RxyTests/MockResultTests.swift
+++ RxyTests/MockResultTests.swift
@@ -7,43 +7,54 @@ import Nimble
 
 class MockResultTests: XCTestCase {
 
-    // MARK: - Completables
+    // MARK: - SingleResult
 
-//    func testCompletableWaitForErrorReturnsError() {
-//        let x: CompletableResult = .throw(RxyError.wrongType)
-//        let completable = x.resolve()
-//        expect(completable.waitForError()).to(matchError(RxyError.wrongType))
-//    }
-//
-//    func testCompletableWaitForSuccess() {
-//        let x: CompletableResult = .success()
-//        let completable = x.resolve()
-//        completable.waitForCompletion()
-//    }
-
-    // MARK: - Singles
-
-    func testSingleWaitForSuccessReturnsError() {
-        let x: SingleResult<Int> = .throw(RxyError.wrongType)
-        let single = x.resolve()
-        expect(single.waitForError()).to(matchError(RxyError.wrongType))
+    func testSingleResultResolvesError() {
+        expect(SingleResult<Int>.throw(RxyError.wrongType).resolve().inBackground().waitForError()).to(matchError(RxyError.wrongType))
     }
 
-    func testSingleWaitForSuccessReturnsValue() {
-        let x: SingleResult<Int> = .value(5)
-        let single = x.resolve()
-        expect(single.waitForSuccess()) == 5
+    func testSingleResultResolvesValue() {
+        expect(SingleResult<Int>.value(5).resolve().inBackground().waitForSuccess()) == 5
     }
 
-    func testSingleWaitForSuccessWhenOptionalReturnsNilValue() {
-        let x: SingleResult<Int?> = .value(nil)
-        let single = x.resolve()
-        expect(single.waitForSuccess()!).to(beNil())
+    func testSingleResultWithOptionalReturnsNilValue() {
+        expect(SingleResult<Int?>.value(nil).resolve().inBackground().waitForSuccess()!).to(beNil())
     }
 
-    func testSingleWaitForSuccessReturnsValueFromClosure() {
-        let x: SingleResult<Int> = .value { return 5 }
-        let single = x.resolve()
-        expect(single.waitForSuccess()) == 5
+    func testSingleResultResolvesClosure() {
+        expect(SingleResult<Int>.value { return 5 }.resolve().inBackground().waitForSuccess()) == 5
     }
+    
+    // MARK: - CompletableResult
+
+    func testCompletableResultResolvesError() {
+        expect(CompletableResult.throw(RxyError.wrongType).resolve().inBackground().waitForError()).to(matchError(RxyError.wrongType))
+    }
+
+    func testCompletableResultResolvesSuccess() {
+        CompletableResult.success().resolve().inBackground().waitForCompletion()
+    }
+    
+    // MARK: - MaybeResult
+    
+    func testMaybeResultResolvesError() {
+        expect(MaybeResult<Int>.throw(RxyError.wrongType).resolve().inBackground().waitForError()).to(matchError(RxyError.wrongType))
+    }
+    
+    func testMaybeResultResolvesValue() {
+        expect(MaybeResult<Int>.value(5).resolve().inBackground().waitForValue()) == 5
+    }
+    
+    func testMaybeResultWithOptionalReturnsNilValue() {
+        expect(MaybeResult<Int?>.value(nil).resolve().inBackground().waitForValue()!).to(beNil())
+    }
+    
+    func testMaybeResultResolvesClosure() {
+        expect(MaybeResult<Int>.value { return 5 }.resolve().inBackground().waitForValue()) == 5
+    }
+
+    func testMaybeResultResolvesSuccess() {
+        MaybeResult<Int>.success().resolve().inBackground().waitForCompletion()
+    }
+    
 }
